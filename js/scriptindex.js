ebe4e5db64b910438a02fa35c02925a81955e831
Magnus <Magnus@Magnuss-MacBook-Air-6.local>
2018-01-30 14:32:30 +0100

    hei jeg har gjort alt selv :)

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -1,18 +1,8 @@
-/* Her er for hamburger menyen */
-function Meny() {
+function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
-    }
-}
-
-		function funksjon() {
-    var x = document.getElementById('p');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
     }
 }
