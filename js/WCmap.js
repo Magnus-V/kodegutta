0ee918eecc17a8a242d8f2274486278b645c6cce
Magnus-V <magnves@gmail.com>
2018-04-24 14:12:43 +0200

    litt ekstra

--- js/WCmap.js
+++ js/WCmap.js
@@ -8,6 +8,19 @@ req.open('GET', url, true);
 req.onload  = function() {
    var jsonResponse = req.response;
 	 console.log(jsonResponse);
-};
+	 var bloodcloth = jsonResponse.entries[0].plassering;
+	 console.log(bloodcloth);
+	 var ol = document.createElement('ol');
+	 document.getElementById('liste').appendChild(ol);
+	 var i;
+	 for(i=0; i < jsonResponse.length; i++) {
+	 		var li = document.createElement('li');
+	 		ul.appendChild(li);
+	 		li.innerHTML = jsonResponse.entries[i].plassering;
+			console.log(jsonResponse.entries[i].plassering);
+	 }
+ };
 req.send(null);
+
+
 }
