f15289e3a022238d1a4e4d75b6f2e8b1b70257b1
Magnus-V <magnves@gmail.com>
2018-04-27 11:23:49 +0200

    eerr

--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 var url = "https://hotell.difi.no/api/json/bergen/dokart?";
 
 var toiletObject = retrieveJSON();
+var listOfLists = [];
 
 function retrieveJSON(){
   var req = new XMLHttpRequest();
@@ -16,11 +17,41 @@ function retrieveJSON(){
 function createNumberedList(){
   var ol = document.createElement('ol');
   document.getElementById('liste').appendChild(ol);
-  for(i=0; i < toiletObject.entries.length ; i++) {
+  for(i=0; i < toiletObject.entries.length; i++) {
      var li = document.createElement('li');
      li.innerHTML = toiletObject.entries[i].plassering;
      ol.appendChild(li);
 };
 
+function searchHerre(){
+  var herreList = [];
+  for(i=0; i < toiletObject.entries.length; i++)
+  if(toiletObject.entries[i].herre != "NULL"){
+    herreList.push(toiletObject.entries[i]);
+  }
+  listOfLists.push(herreList);
+}
+
+function searchDame(){
+  var dameList = [];
+  for(i=0; i<toiletObject.entries.length; i++){
+    if(toiletObject.entries[i].dame !="NULL")
+    dameList.push(toiletObject.entries[i]);
+  }
+  listOfLists.push(dameList);
+}
+
+
+function simpleSearch(){
+  resultList = [];
+  var regexAdresse =
+  var regexDame =
+  var regexPissoir =
+  var regexPlassering =
+  var regexPris =
+  var regexRullestol =
+  var regexStellerom =
+
+}
 
 }
